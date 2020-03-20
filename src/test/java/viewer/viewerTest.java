d23d8c8757d4804249812d677459b7e16e0d0b3b
Audi Bailey <audibailey7@gmail.com>
2020-03-20 18:07:54 +1000

    refactor: Reimplement CI as per Maven and BitBucket specifications.

--- src/test/java/viewer/viewerTest.java
+++ src/test/java/viewer/viewerTest.java
@@ -5,22 +5,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.function.Executable;
-import viewer.Main.*;
+import viewer.*;
 
 public class viewerTest {
     @Test
-    public void testGreaterThaZero() throws Exception {
+    public void greaterThanZeroTestCase() throws Exception {
         assertEquals(Main.ExampleFunction(1), 1);
     }
 
     @Test
-    public void testEqualZero() {
+    public void equalZeroTestCase() {
         assertThrows(Exception.class, () -> Main.ExampleFunction(0));
     }
 
     @Test
-    public void testLessThanZero() {
+    public void lessThanZeroTestCase() {
         assertThrows(Exception.class, () -> Main.ExampleFunction(-1));
     }
 }
