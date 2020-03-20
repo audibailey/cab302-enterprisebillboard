69673006c9807e7938b73313ad5671dbfe5f9550
Audi Bailey <audibailey7@gmail.com>
2020-03-20 14:16:05 +1000

    Ported from Github.

---
+++ src/client/Main.java
@@ -0,0 +1,13 @@
+package client;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        // write your code here
+
+        while (true) {
+            System.out.println("Client");
+            Thread.sleep(1000);
+        }
+    }
+}
