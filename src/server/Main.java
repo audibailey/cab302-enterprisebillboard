16be11e0c4586d5b9d2b0e5926cdb2a44eb1573e
Perdana Bailey <perdana.bailey@connect.qut.edu.au>
2020-03-20 14:16:05 +1000

    Ported from Github.

---
+++ src/server/Main.java
@@ -0,0 +1,13 @@
+package server;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+	    // write your code here
+
+        while (true) {
+            System.out.println("Server");
+            Thread.sleep(1000);
+        }
+    }
+}
---
+++ src/viewer/Main.java
@@ -0,0 +1,13 @@
+package viewer;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        // write your code here
+
+        while (true) {
+            System.out.println("Viewer");
+            Thread.sleep(1000);
+        }
+    }
+}
