3ebb1255081aa6d0b21107f491c09ece2e6edf12
Jamie Martin <49264143+JamieKieranMartin@users.noreply.github.com>
2020-04-04 22:06:18 +1000

    fix: remove unneccessary code

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -5,9 +5,5 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 	    // write your code here
 
-        while (true) {
-            System.out.println("Server");
-            Thread.sleep(1000);
-        }
-    }
+     }
 }
