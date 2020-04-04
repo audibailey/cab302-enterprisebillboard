80031be5d59ea3b327c9a266c45584e6c5ff1761
Perdana Bailey <perdana.bailey@connect.qut.edu.au>
2020-04-04 22:04:37 +1000

    refactor: Changed the way creation of a new database object works.

--- src/main/java/server/database/DB.java
+++ src/main/java/server/database/DB.java
@@ -2,41 +2,70 @@ package server.database;
 
 import java.sql.*;
 
+/**
+ * This class consists of the database handler. All methods that require direct access
+ * to the database are present in this file, along with the database connection and any
+ * other methods that work with the database connection.
+ *
+ * @author Perdana Bailey
+ * @author Kevin Huynh
+ */
 public class DB {
+    /**
+     * Main point of contact for the database
+     */
     private Connection database;
 
-    public DB(String url, String username, String password) throws SQLException {
+    /**
+     * Creates the database object and connects to the database management software.
+     * If the DBMS doesn't have the database, create the database and update
+     * the object to use that database as the connection.
+     *
+     * @param url:          the DBMS url. Example: jdbc:mysql://127.0.0.1
+     * @param username:     the DBMS username
+     * @param password:     the DBMS password
+     * @param databaseName: the database name
+     * @throws SQLException: this exception is just a pass-through
+     */
+    public DB(String url, String username, String password, String databaseName) throws Exception {
+        // Set the JDBC driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
 
+        // Connect to the DBMS and populate the schema
         this.database = DriverManager.getConnection(url, username, password);
-
-        // Checks Database Exists
-        ResultSet resultSet = this.database.getMetaData().getCatalogs();
-        boolean databaseExists = false;
-
-        while (resultSet.next()) {
-            String databaseName = resultSet.getString(1);
-
-            if (databaseName.equals("30221")) {
-                databaseExists = true;
-                break;
-            }
-        }
-        resultSet.close();
-
-        if (!databaseExists) {
-            populateSchema();
-            this.database = DriverManager.getConnection(url + "/30221", username, password);
-        } else {
-            this.database = DriverManager.getConnection(url + "/30221", username, password);
-        }
+        populateSchema(url, username, password, databaseName);
     }
 
-    private void populateSchema() throws SQLException {
-        Statement stmt = this.database.createStatement();
-        stmt.executeUpdate("CREATE TABLE a (id int not null primary key, value varchar(20))");
-        stmt.close();
+    /**
+     * Create database, populate the database  and switch the connection to use that
+     *
+     * @param url:          the DBMS url. Example: jdbc:mysql://127.0.0.1
+     * @param username:     the DBMS password
+     * @param password:     the DBMS password
+     * @param databaseName: the database name
+     * @throws SQLException: this exception is just a pass-through
+     */
+    private void populateSchema(String url, String username, String password, String databaseName) throws SQLException {
+        // Create an SQL statement
+        Statement sqlStatement = this.database.createStatement();
+
+        // Create and test new database
+        sqlStatement.executeUpdate("CREATE DATABASE IF NOT EXISTS " + databaseName);
+        sqlStatement.executeUpdate("USE " + databaseName);
+
+        // End the SQL statement
+        sqlStatement.close();
+
+        // Set the new handler to the new database
+        this.database = DriverManager.getConnection(url + "/" + databaseName, username, password);
     }
 
+    /**
+     * Closes the connection to the database.
+     * <b>WARNING THIS WILL RENDER THE DB OBJECT USELESS!</b>
+     *
+     * @throws SQLException, this exception is just a pass-through
+     */
     public void closeConnection() throws SQLException {
         this.database.close();
     }
