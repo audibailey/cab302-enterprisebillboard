adf7508e1e61808f7794d167c0009db648aa5005
Audi Bailey <audibailey7@gmail.com>
2020-04-04 22:03:03 +1000

    test: Add a basic database handler test.

---
+++ src/test/java/server/database/DBTest.java
@@ -0,0 +1,24 @@
+package server.database;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import server.database.*;
+
+public class DBTest {
+
+    DB testDB;
+
+    @BeforeEach
+    public void createDatabase() throws Exception {
+        this.testDB = new DB("jdbc:mysql://localhost:3306",
+            "root",
+            "password",
+            "cab302");
+    }
+
+    @Test
+    public void testClose() throws Exception {
+        this.testDB.closeConnection();
+    }
+}
