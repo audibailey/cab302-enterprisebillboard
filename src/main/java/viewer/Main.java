718e100496a2604b09370475171c67f420fd7cd4
Trevor Waturuocha <trevor.waturuocha@connect.qut.edu.au>
2020-04-05 20:19:13 +1000

    feat: Fullscreen application and Escape key // Click to Exit
    ‘Escape’ key or clicking the mouse anywhere on the billboard screen closes the billboard viewer. Nothing else visible on the screen except the contents of the currently displayed billboard- no window borders.

--- src/main/java/viewer/Main.java
+++ src/main/java/viewer/Main.java
@@ -1,17 +1,41 @@
 package viewer;
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
 
-    public static void main(String[] args) throws InterruptedException {
-        // write your code here
-        new Frame("Hello World!");
+public class Main {
+    // Class to create and display GUI
+    private static void createAndShowGUI () {
+        JFrame frame = new JFrame("Billboard Viewer"); // Constructing Billboard Viewer frame
+
+        // Get the screen dimensions
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+
+        // Setting the frame dimensions
+        final int screen_Width = dim.width; // Screen width
+        final int screen_Height = dim.height; // Screen height
+        frame.setSize(screen_Width, screen_Height); // Setting frame size
+
+        // Setting the frame event listeners
+        frame.addKeyListener(new ExitEvents.KeyListener()); // Adding key listener
+        frame.addMouseListener(new ExitEvents.MouseListener()); // Adding mouse listener
+
+        // Setting frame properties
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Set frame to exit on close
+        frame.setExtendedState(Frame.MAXIMIZED_BOTH); // Setting frame size to maximise to full screen
+        frame.setUndecorated(true); // Removing the frame title bar including default buttons
+        frame.setContentPane (new ViewerPanel ()); // Assigning Viewer panel to Viewer frame
+        frame.setVisible (true); // Show frame
     }
 
-    public static int ExampleFunction(int num) throws Exception {
-        if (num > 0) {
-            return num;
-        } else {
-            throw new Exception("Negative or 0 not valid");
-        }
+    // Main class to run GUI Application
+    public static void main(String[] args) throws InterruptedException {
+        // Schedule a job for the event-dispatching thread:
+        // creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
     }
 }
