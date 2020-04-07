3e5cab11bb0052b9683c5d87ead7734c8716d8c1
Jamie Martin <jamie.martin@connect.qut.edu.au>
2020-04-08 09:23:37 +1000

    feat: Req/Res object classes, refactor: ConnectionFactory to Dataservice, DB.java to garbage

---
+++ src/main/java/common/models/Request.java
@@ -0,0 +1,30 @@
+package common.models;
+
+public class Request {
+    // Obvs these need fixing up
+    String route;
+    String method;
+    String data;
+
+    public Request() {
+
+    }
+
+    /**
+     * Parses the XMl string and returns a new Request
+     * @param xml
+     * @return Request
+     */
+    public static Request fromXML(String xml) {
+        return new Request();
+    }
+
+    /**
+     * Parses the Object and returns an XML string
+     * @param r
+     * @return String
+     */
+    public static String fromObject(Request r) {
+        return "";
+    }
+}
--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -1,9 +1,21 @@
 package server;
 
+import common.models.Billboard;
+import server.database.DataService;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
 	    // write your code here
+
+        DataService dataService = new DataService();
+
+        dataService.billboards.getAll();
+        dataService.users.getAll();
+        dataService.schedules.getAll();
+
+        // Billboard.fromObject(dataService.billboards.get("Billboard 1"));
 
         while (true) {
             System.out.println("Server");
