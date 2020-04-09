4057bb71ea663f6fd310dc2ad77812f8affd3a28
Perdana Bailey <perdana.bailey@connect.qut.edu.au>
2020-04-09 21:49:25 +1000

    feat: Implement mock database ID increment.

--- src/main/java/server/database/user/UserHandler.java
+++ src/main/java/server/database/user/UserHandler.java
@@ -25,6 +25,7 @@ public class UserHandler implements ObjectHandler<User> {
 
     // This is the mock "database" used for testing
     List<User> MockDB = new ArrayList<User>();
+    int MockDBNum = 0;
 
     /**
      * The UserHandler Constructor.
@@ -36,26 +37,33 @@ public class UserHandler implements ObjectHandler<User> {
     }
 
     /**
-     * Selects a user in the database based off user name.
+     * Selects a user in the database based off id.
      *
      * @param id: the id of the user
-     * @return
-     * @throws Exception: this exception is a pass-through exception with a no results extended exception
+     * @return Optional<User>: this returns the user or an optional empty value.
+     * @throws SQLException: this exception is a pass-through exception with a no results extended exception
      */
-    public Optional<User> get(int id) throws Exception {
+    public Optional<User> get(int id) throws SQLException {
         // Check that it's not in testing mode
         if (this.connection != null) {
+            // Initialise return value
+            Optional<User> ReturnUser = Optional.empty();
 
+            // Attempt to query the database
             Statement sqlStatement = connection.createStatement();
-
             ResultSet result = sqlStatement.executeQuery("SELECT * FROM USERS WHERE id = " + id);
 
+            // Use the result of the database query to create the User object and return it
             while (result.next()) {
-                return Optional.of(User.fromSQL(result));
-
+                ReturnUser = Optional.of(User.fromSQL(result));
             }
+
+            // Clean up query
             sqlStatement.close();
+
+            return ReturnUser;
         } else {
+            // Loop through and find the user with the requested name or return an optional empty value
             for (User u : this.MockDB) {
                 if (u.id == id) {
                     return Optional.of(u);
@@ -75,19 +83,27 @@ public class UserHandler implements ObjectHandler<User> {
     public Optional<User> get(String username) throws SQLException {
         // Check that it's not in testing mode
         if (this.connection != null) {
+            // Initialise return value
+            Optional<User> ReturnUser = Optional.empty();
+
             // Attempt to query the database
             Statement sqlStatement = this.connection.createStatement();
-            // Create a query that selects billboards based on the name and execute the query
+
+            // Create a query that selects user based on the name and execute the query
             String query = "SELECT * FROM USERS WHERE username = '" + username + "'";
             ResultSet result = sqlStatement.executeQuery(query);
 
-            // Use the result of the database query to create billboard object and return it
+            // Use the result of the database query to create the User object and return it
             while (result.next()) {
-                return Optional.of(User.fromSQL(result));
+                ReturnUser = Optional.of(User.fromSQL(result));
             }
+
+            // Clean up query
             sqlStatement.close();
+
+            return ReturnUser;
         } else {
-            // Loop through and find the billboard with the requested name or return an optional empty value
+            // Loop through and find the user with the requested name or return an optional empty value
             for (User user : this.MockDB) {
                 if (user.username.equals(username)) {
                     return Optional.of(user);
@@ -103,21 +119,25 @@ public class UserHandler implements ObjectHandler<User> {
      *
      * @throws Exception: this exception is a pass-through exception with a no results extended exception
      */
-    public List<User> getAll() throws Exception {
+    public List<User> getAll() throws SQLException {
         // User's list to be returned
         List<User> users = new ArrayList<>();
+
+        // Check that it's not in testing mode
         if (this.connection != null) {
 
-            // Query the database for the billboard
+            // Attempt to query the database
             Statement sqlStatement = connection.createStatement();
 
-            // Use the result of the database to create billboard object
+            // Create a query that selects all users and execute the query
             ResultSet result = sqlStatement.executeQuery("SELECT * FROM USERS");
 
+            // Use the result of the database query to create user object and add it to the returned list
             while (result.next()) {
                 users.add(User.fromSQL(result));
             }
 
+            // Clean up query
             sqlStatement.close();
         } else {
             users = this.MockDB;
@@ -127,40 +147,60 @@ public class UserHandler implements ObjectHandler<User> {
     }
 
     /**
+     * Insert a user into the database.
      *
-     * @param user
-     * @throws Exception
+     * @param user: this is the requested user to insert.
+     * @throws SQLException: this exception returns when there is an issue sending data to the database.
      */
-    public void insert(User user) throws Exception {
+    public void insert(User user) throws SQLException {
+
+        // Check that it's not in testing mode
         if (this.connection != null) {
+            // Attempt to query the database
             Statement sqlStatement = connection.createStatement();
 
+            // Create a query that adds the user and execute the query
             sqlStatement.executeUpdate(
-            "INSERT INTO USERS " +
-                 "(username, password, salt)" +
-                 "VALUES( " + user.username +
-                 "," + user.password +
-                 "," + user.salt + ")");
+                "INSERT INTO USERS " +
+                    "(username, password, salt)" +
+                    "VALUES( '" + user.username +
+                    "','" + user.password +
+                    "','" + user.salt + "')");
 
+            // Clean up query
             sqlStatement.close();
         } else {
+            user.id = MockDBNum;
             this.MockDB.add(user);
+            MockDBNum++;
         }
     }
 
     /**
+     * Update a user in the database.
      *
-     * @param user
-     * @throws Exception
+     * @param user: this is the requested user to update.
+     * @throws SQLException: this exception returns when there is an issue sending data to the database.
      */
-    public void update(User user) throws Exception {
+    public void update(User user) throws SQLException {
+
+        // Check that it's not in testing mode
         if (this.connection != null) {
+            // Attempt to query the database
             Statement sqlStatement = connection.createStatement();
 
-            sqlStatement.executeUpdate("");
+            // Create a query that updates the user and execute the query
+            String query = "UPDATE users SET password =" +
+                user.password +
+                ", salt =" + user.salt +
+                " WHERE ID = " + user.id;
 
+            sqlStatement.executeUpdate(query);
+
+            // Clean up query
             sqlStatement.close();
         } else {
+            // Loop through mock database and find the user to update, then update it
             for (User mockUser : this.MockDB) {
                 if (mockUser.id == user.id) {
                     mockUser = user;
@@ -170,17 +210,22 @@ public class UserHandler implements ObjectHandler<User> {
     }
 
     /**
-     * Delete a user base on userName
+     * Delete a user from the database.
      *
-     * @param user: the content of the user
-     * @throws Exception: this exception is a pass-through exception with a no results extended exception
+     * @param user: this is the requested user to delete.
+     * @throws SQLException: this exception is thrown when there is an issue deleting data from the database.
      */
-    public void delete(User user) throws Exception {
+    public void delete(User user) throws SQLException {
+
+        // Check that it's not in testing mode
         if (this.connection != null) {
+            // Attempt to query the database
             Statement sqlStatement = connection.createStatement();
 
+            // Create a query that deletes the user and executes the query
             sqlStatement.executeUpdate("DELETE FROM USERS WHERE id = " + user.id);
 
+            // Clean up query
             sqlStatement.close();
         } else {
             this.MockDB.remove(user);
