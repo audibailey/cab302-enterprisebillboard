075fa40f5e10454f6ceff2be43bff009bcca8374
Kevin <jack6969x@gmail.com>
2020-04-09 17:43:28 +1000

    Add tests in UserTests

--- src/test/java/server/database/users/UserTests.java
+++ src/test/java/server/database/users/UserTests.java
@@ -1,5 +1,6 @@
 package server.database.users;
 
+import common.models.Billboard;
 import common.models.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -17,12 +18,13 @@ import static org.junit.jupiter.api.Assertions.*;
  * Test class for the user database handler
  *
  * @author Jamie Martin
+ * @author Kevin Huynh
  */
 public class UserTests {
 
     // The DataService used by all the tests, essentially a database connection.
     private static DataService dataService;
-    List<User> controlUsers = new ArrayList<User>();
+    static List<User> controlUsers = new ArrayList<r>();
 
     /**
      * Connects to the database or a mock database.
@@ -35,12 +37,62 @@ public class UserTests {
     }
 
     @Test
-    public void GetByIdNewUser() throws Exception {
+    public void GetUserByID() throws Exception {
+        User user = new User(22, "Username", "pass", "salt");
+        User user1 = new User(33, "User1", "pass", "salt");
+        dataService.users.insert(user);
+        dataService.users.insert(user1);
+        controlUsers.add(user);
+        controlUsers.add(user1);
+
+        Optional<User> GotUser = dataService.users.get(22);
+        if (GotUser.isPresent()) {
+            // Test the retrieve user ID against the control ID
+            assertEquals(GotUser.get().id, 22);
+
+            // Cleanup and delete the user
+            dataService.users.delete(GotUser.get());
+        } else {
+            fail("Error fetching user.");
+        }
+    }
+
+    @Test
+    public void GetAllUsers() throws Exception {
+        User user = new User(1, "Username", "pass", "salt");
+        User user1 = new User(2, "Username2", "pass", "salt");
+        User user2 = new User(3, "Username3", "pass", "salt");
+        User user3 = new User(4, "Username4", "pass", "salt");
+        dataService.users.insert(user);
+        dataService.users.insert(user1);
+        dataService.users.insert(user2);
+        dataService.users.insert(user3);
+        List<User> result = dataService.users.getAll();
+        controlUsers.add(user);
+        controlUsers.add(user1);
+        controlUsers.add(user2);
+        controlUsers.add(user3);
+        System.out.println(result);
+        assertTrue(controlUsers.containsAll(dataService.users.getAll()));
 
     }
 
     @Test
-    public void GetByIdExistingUser() throws Exception {
+    public void GetUserByUsername() throws Exception {
+        User user = new User(12, "Username12", "pass", "salt");
+        User user1 = new User(13, "User12", "pass", "salt");
+        User user2 = new User(14, "AnotherUser", "pass", "salt");
+        dataService.users.insert(user);
+        dataService.users.insert(user1);
+        dataService.users.insert(user2);
+
+        Optional<User> GotUser = dataService.users.get("User12");
+        if (GotUser.isPresent()) {
+            // Test the retrieve username against the control username
+            assertEquals(GotUser.get().username, "User12");
+        } else {
+            fail("Error fetching user.");
+        }
 
     }
 
@@ -56,7 +108,7 @@ public class UserTests {
 
     @Test
     public void InsertNewUser() throws Exception {
-        User user = new User(5, "User", "Password", "Salt");
+        User user = new User(5, "Jamie", "Password", "Salt");
         dataService.users.insert(user);
         controlUsers.add(user);
         assertTrue(controlUsers.containsAll(dataService.users.getAll()));
@@ -79,7 +131,20 @@ public class UserTests {
 
     @Test
     public void DeleteNewUser() throws Exception {
+        User user = new User(4, "User5", "Password", "Salt");
+        dataService.users.insert(user);
 
+        // Retrieve the testing billboard then delete it
+        Optional<User> ToDeleteUser = dataService.users.get(4);
+        if (ToDeleteUser.isPresent()) {
+            dataService.users.delete(ToDeleteUser.get());
+
+            // Attempt to retrieve the testing billboard and ensure nothing is returned
+            Optional<User> DeletedUser = dataService.users.get(4);
+            assertTrue(DeletedUser.isEmpty());
+        } else {
+            fail("Error fetching to be deleted billboard.");
+        }
     }
 
     @Test
