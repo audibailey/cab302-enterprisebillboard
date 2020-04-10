53f28b124dc3c1775190561c1b57c0b2e3088d1c
Jamie Martin <49264143+JamieKieranMartin@users.noreply.github.com>
2020-04-10 18:18:25 +1000

    mutiple: see description
    refactor: login and client frames and get them functional
    feat: tables, and generic selectable table for billboard and users
    refactor: remove unnneccessary client stuffs

---
+++ src/main/java/client/components/Menu.java
@@ -0,0 +1,36 @@
+package client.components;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Menu extends JMenuBar {
+    Color bgColor=Color.WHITE;
+
+    private JMenuItem logout = new JMenuItem("Logout") {
+        @Override
+        public Dimension getMaximumSize() {
+            setBackground(bgColor);
+            Dimension d1 = super.getPreferredSize();
+            Dimension d2 = super.getMaximumSize();
+            d2.width = d1.width;
+            return d2;
+        }
+    };
+
+    public Menu() {
+        add(Box.createHorizontalGlue());
+        add(logout);
+    }
+
+    public JMenuItem getLogout() {
+        return logout;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(bgColor);
+        g2d.fillRect(0, 0, getWidth() - 1, getHeight() - 1);
+    }
+}
