d3396a29c004b075b22636d312014d3fbdafe0b0
Jamie Martin <jamie.martin@connect.qut.edu.au>
2020-04-15 16:29:29 +1000

    feat: Notifcation button tests

--- src/main/java/client/panels/SchedulePanel.java
+++ src/main/java/client/panels/SchedulePanel.java
@@ -1,6 +1,8 @@
 package client.panels;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class SchedulePanel extends JPanel {
     JButton button;
@@ -8,6 +10,19 @@ public class SchedulePanel extends JPanel {
     public SchedulePanel() {
 
         button = new JButton("Schedule");
+        button.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                common.swing.Notification.display("Hello", "World!", JOptionPane.INFORMATION_MESSAGE);
+                try {
+                    common.system.Notification.display("Hello", "test", TrayIcon.MessageType.ERROR);
+                } catch (AWTException awtException) {
+                    awtException.printStackTrace();
+                }
+            }
+        });
         add(button);
 
     }
