4a1a234def8a03e052e5197d5249c484421ee6d5
Perdana Bailey <perdana.bailey@connect.qut.edu.au>
2020-04-20 15:41:10 +1000

    refactor: Standardise the Request and Response object formats.

--- src/main/java/common/models/Request.java
+++ src/main/java/common/models/Request.java
@@ -1,21 +1,41 @@
 package common.models;
 
-import common.Method;
-
 import java.io.Serializable;
 
-public class Request<T> implements Serializable {
-    // Obvs these need fixing up
-    public Method method;
-    public T data;
-    public Object params;
+import common.Methods;
 
+/**
+ * This class is the Request object.
+ *
+ * @author Perdana Bailey
+ * @author Jamie Martin
+ */
+public class Request<T> implements Serializable {
+
+    /**
+     * The variables of the object Request.
+     */
+    public Methods method;
+    public String token;
+    public T data;
+
+    /**
+     * An empty constructor just for creating the object.
+     */
     public Request() {
 
     }
 
-    public Request(Method method, T data) {
+    /**
+     * Constructor for the Request object.
+     *
+     * @param method: An enum that determines the route the clients request will take.
+     * @param token:  A token to authenticate the clients request.
+     * @param data:   This acts as the parameter of the request.
+     */
+    public Request(Methods method, String token, T data) {
         this.method = method;
+        this.token = token;
         this.data = data;
     }
 }
--- src/main/java/common/models/Response.java
+++ src/main/java/common/models/Response.java
@@ -1,18 +1,36 @@
 package common.models;
 
 import java.io.Serializable;
-import java.util.List;
 
+import common.Status;
+
+/**
+ * This class is the Response object.
+ *
+ * @author Perdana Bailey
+ * @author Jamie Martin
+ */
 public class Response<T> implements Serializable {
-    // Obvs these need fixing up
+    /**
+     * The variables of the object billboard.
+     */
     public Status status;
-    public List<T> data;
+    public T data;
 
+    /**
+     * An empty constructor just for creating the object.
+     */
     public Response() {
 
     }
 
-    public Response(List<T> data, Status status) {
+    /**
+     * Constructor for the Response object.
+     *
+     * @param status: An enum that determines if the request was successful.
+     * @param data:   This acts as the result of the request.
+     */
+    public Response(Status status, T data) {
         this.status = status;
         this.data = data;
     }
