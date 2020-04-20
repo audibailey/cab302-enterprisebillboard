9cba806a6c804b5859ce73c884db0c5040405da6
Audi Bailey <audibailey7@gmail.com>
2020-04-20 15:41:10 +1000

    refactor: Standardise the Request and Response object formats.

---
+++ src/main/java/common/Methods.java
@@ -0,0 +1,14 @@
+package common;
+
+/**
+ * This enum determines the routes the requests will take.
+ *
+ * @author Perdana Bailey
+ * @author Jamie Martin
+ */
+public enum Methods {
+    GET_BILLBOARDS,
+    POST_BILLBOARD,
+    UPDATE_BILLBOARD,
+    DELETE_BILLBOARD,
+}
---
+++ src/main/java/common/Status.java
@@ -0,0 +1,11 @@
+package common;
+
+/**
+ * This enum determines the type of response.
+ *
+ * @author Perdana Bailey
+ */
+public enum Status {
+    SUCCESS,
+    FAILED,
+}
