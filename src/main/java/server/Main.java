e64f3c183275b81018bce31c123a2521faf059cd
Perdana Bailey <perdana.bailey@connect.qut.edu.au>
2020-04-20 15:42:16 +1000

    refactor: Move the object initialisations to the correct place.

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -1,18 +1,41 @@
 package server;
 
-import server.endpoints.ClientHandler;
-
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import server.database.DataService;
+import server.endpoints.ClientHandler;
+import server.endpoints.EndpointHandler;
+
+/**
+ * This class is the main class, used as the entry point for the server application.
+ *
+ * @author Perdana Bailey
+ * @author Jamie Martin
+ */
 public class Main {
 
+    /**
+     * This is the main function.
+     *
+     * @param args: command line arguments.
+     * @throws Exception: Automatically critical fail if an error is thrown.
+     */
     public static void main(String[] args) throws Exception {
+        // Connect and populate the database
+        DataService dataService = new DataService(false);
+
+        // Connect the endpoint routes to the database handler
+        EndpointHandler endpointHandler = new EndpointHandler(dataService);
+
+        // Open the socket
         ServerSocket ss = new ServerSocket(12345);
 
+        // Loop through constantly looking for connections
         while (true) {
+            // When a connection is found accept it and create a thread for it
             Socket s = ss.accept();
-            new Thread(new ClientHandler(s.getInputStream(), s.getOutputStream())).start();
+            new Thread(new ClientHandler(s, endpointHandler)).start();
         }
     }
 }
