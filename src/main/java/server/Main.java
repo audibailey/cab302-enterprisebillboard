8f1e27d5b0e8575b0f96a6616ee46936d4c78bc1
Perdana Bailey <perdana.bailey@connect.qut.edu.au>
2020-04-21 23:22:59 +1000

    feat: Implemented the first iteration of middleware with a client example.

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -6,6 +6,7 @@ import java.net.Socket;
 import server.database.DataService;
 import server.endpoints.ClientHandler;
 import server.endpoints.EndpointHandler;
+import server.middleware.MiddlewareHandler;
 
 /**
  * This class is the main class, used as the entry point for the server application.
@@ -22,19 +23,31 @@ public class Main {
      * @throws Exception: Automatically critical fail if an error is thrown.
      */
     public static void main(String[] args) throws Exception {
+        System.out.println("Billboard Server");
+
+        System.out.println("Attempting to connect to database...");
         // Connect and populate the database
         DataService dataService = new DataService(false);
 
-        // Connect the endpoint routes to the database handler
-        EndpointHandler endpointHandler = new EndpointHandler(dataService);
+        // Connect middleware with the database
+        System.out.println("Attempting to connect middleware to database...");
+        MiddlewareHandler middlewareHandler = new MiddlewareHandler(dataService);
+
+        // Connect the endpoint routes to the database handler and middleware
+        System.out.println("Attempting to sync endpoints with the database and middleware...");
+        EndpointHandler endpointHandler = new EndpointHandler(dataService, middlewareHandler);
+        System.out.println("All required services are online and linked.");
 
         // Open the socket
+        System.out.println("Opening Server on port 12345...");
         ServerSocket ss = new ServerSocket(12345);
+        System.out.println("Sever available at " + ss.getLocalSocketAddress());
 
         // Loop through constantly looking for connections
         while (true) {
             // When a connection is found accept it and create a thread for it
             Socket s = ss.accept();
+            System.out.println("A request attempt has been made from " + s.getInetAddress());
             new Thread(new ClientHandler(s, endpointHandler)).start();
         }
     }
