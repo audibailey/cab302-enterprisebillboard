b72c6507a0ad59ecd5c99e183b96fb013b38d75f
Hieu Nghia Huynh <hieunghia.huynh@connect.qut.edu.au>
2020-05-08 21:02:50 +1000

    feat: Added permission endpoints + add function to check token's user against request.data user in MiddlewareHandler.java

--- src/main/java/server/endpoints/permission/UpdatePermissionHandler.java
+++ src/main/java/server/endpoints/permission/UpdatePermissionHandler.java
@@ -1,51 +1,51 @@
-package server.endpoints.user;
+package server.endpoints.permission;
 
 import java.util.Optional;
 
+import common.models.Permissions;
 import common.models.Response;
 import common.Status;
-import common.models.User;
 import server.database.DataService;
 
 /**
- * This class handles the delete function of user database handler and turns it into a response
+ * This class handles the update function of permission database handler and turns it into a response
  * for the client.
  *
  * @author Perdana Bailey
  * @author Kevin Huynh
  */
-public class UpdateUserHandler {
+public class UpdatePermissionHandler {
     /**
      * Update the user and return a response
      *
      * @param db: This is used to call the database handler.
      * @return Response<?>: This is the response to send back to the client.
      */
-    public static Response<?> updateUser(DataService db, User user) {
+    public static Response<?> updatePermission(DataService db, Permissions permissions) {
         try {
             // Get the user before update
-            Optional<User> beforeUpdate = db.users.get(user.username);
-            db.users.update(user);
+            Optional<Permissions> beforeUpdate = db.permissions.get(permissions.username);
+            db.permissions.update(permissions);
             // Attempt to get the updated user
-            Optional<User> updatedUser = db.users.get(user.username);
+            Optional<Permissions> updatedPerm = db.permissions.get(permissions.username);
             // Check if the user has been update or not
-            if (!beforeUpdate.equals(updatedUser)) {
+            if (!beforeUpdate.equals(updatedPerm)) {
                 // Return a success  message
                 return new Response<>(
                     Status.SUCCESS,
-                    "User successfully updated."
+                    "Permission successfully updated."
                 );
             } else {
                 // Return a failed message
                 return new Response<>(
                     Status.INTERNAL_SERVER_ERROR,
-                    "There was an error updating the user."
+                    "There was an error updating the permission."
                 );
             }
         } catch (Exception e) {
             // TODO: Console Log this
             // If an issue occurs return a failed with the error message as the exception
-            return new Response<>(Status.INTERNAL_SERVER_ERROR, "Failed to delete user from the database.");
+            return new Response<>(Status.INTERNAL_SERVER_ERROR, "Failed to delete permission from the database.");
         }
     }
 }
