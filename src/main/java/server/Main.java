04716d58f795c52c2eb4250cd8cd06a8e1f430b8
Jamie Martin <jamie.martin@connect.qut.edu.au>
2020-05-09 22:02:14 +1000

    refactor: server main method to implement new features + cleanup

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -1,12 +1,13 @@
 package server;
 
+import server.controllers.billboard.Get;
+import server.controllers.billboard.Insert;
+import server.controllers.billboard.Update;
+import server.router.*;
+import server.services.DataService;
+
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import server.database.DataService;
-import server.endpoints.ClientHandler;
-import server.endpoints.EndpointHandler;
-import server.middleware.MiddlewareHandler;
 
 /**
  * This class is the main class, used as the entry point for the server application.
@@ -27,16 +28,13 @@ public class Main {
 
         System.out.println("Attempting to connect to database...");
         // Connect and populate the database
-        DataService dataService = new DataService(false);
+        DataService.getConnection();
 
-        // Connect middleware with the database
-        System.out.println("Attempting to connect middleware to database...");
-        MiddlewareHandler middlewareHandler = new MiddlewareHandler(dataService);
-
-        // Connect the endpoint routes to the database handler and middleware
-        System.out.println("Attempting to sync endpoints with the database and middleware...");
-        EndpointHandler endpointHandler = new EndpointHandler(dataService, middlewareHandler);
-        System.out.println("All required services are online and linked.");
+        // ADD THE ROUTER
+        Router router = new Router().ADD("/billboard/get", Get.class)
+            .ADD("/billboard/getbyid", Get.class)
+            .ADD("billboard/insert", Insert.class)
+            .ADD("/billboard/update", Update.class);
 
         // Open the socket
         System.out.println("Opening Server on port 12345...");
@@ -48,7 +46,7 @@ public class Main {
             // When a connection is found accept it and create a thread for it
             Socket s = ss.accept();
             System.out.println("A request attempt has been made from " + s.getInetAddress());
-            new Thread(new ClientHandler(s, endpointHandler)).start();
+            new Thread(new ClientHandler(s, router)).start();
         }
     }
 }
