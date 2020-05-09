69824bde5288dfc94e7b77f7ea702351fccc18af
Jamie Martin <jamie.martin@connect.qut.edu.au>
2020-05-09 22:00:54 +1000

    refactor: comment out old monolithic handlers

--- src/main/java/server/endpoints/user/PostUserHandler.java
+++ src/main/java/server/endpoints/user/PostUserHandler.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import common.models.Response;
 import common.Status;
 import common.models.User;
-import server.database.DataService;
 
 /**
  * This class handles the post function of user database handler and turns it into a response
@@ -15,35 +14,35 @@ import server.database.DataService;
  * @author Kevin Huynh
  */
 public class PostUserHandler {
-    /**
-     * Insert the user and return a response
-     *
-     * @param db:   This is used to call the database handler.
-     * @param user: This is used to pass the user data
-     * @return Response<?>: This is the response to send back to the client.
-     */
-    public static Response<?> insertUser(DataService db, User user) {
-        try {
-            db.users.insert(user);
-            // Attempt to get the inserted user
-            Optional<User> insertedUser = db.users.get(user.username);
-
-            if (insertedUser.isPresent()) {
-                // Return success code
-                return new Response<>(
-                    Status.SUCCESS,
-                    "User successfully inserted."
-                );
-            } else {
-                return new Response<>(
-                    Status.INTERNAL_SERVER_ERROR,
-                    "User did not insert."
-                );
-            }
-        } catch (Exception e) {
-            // TODO: Console Log this
-            // If an issue occurs return a failed with the error message as the exception
-            return new Response<>(Status.INTERNAL_SERVER_ERROR, "Failed to get insert user into the database.");
-        }
-    }
+//    /**
+//     * Insert the user and return a response
+//     *
+//     * @param db:   This is used to call the database handler.
+//     * @param user: This is used to pass the user data
+//     * @return Response<?>: This is the response to send back to the client.
+//     */
+//    public static Response<?> insertUser(DataService db, User user) {
+//        try {
+//            db.users.insert(user);
+//            // Attempt to get the inserted user
+//            Optional<User> insertedUser = db.users.get(user.username);
+//
+//            if (insertedUser.isPresent()) {
+//                // Return success code
+//                return new Response<>(
+//                    Status.SUCCESS,
+//                    "User successfully inserted."
+//                );
+//            } else {
+//                return new Response<>(
+//                    Status.INTERNAL_SERVER_ERROR,
+//                    "User did not insert."
+//                );
+//            }
+//        } catch (Exception e) {
+//            // TODO: Console Log this
+//            // If an issue occurs return a failed with the error message as the exception
+//            return new Response<>(Status.INTERNAL_SERVER_ERROR, "Failed to get insert user into the database.");
+//        }
+//    }
 }
