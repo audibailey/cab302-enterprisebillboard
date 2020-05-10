16a39d0602cb57326d0b74d240cc150109b2549e
Audi Bailey <audibailey7@gmail.com>
2020-05-10 15:34:51 +1000

    docs: Documentation for the classes.

--- src/main/java/common/router/BadRequest.java
+++ src/main/java/common/router/BadRequest.java
@@ -1,6 +1,13 @@
 package common.router;
 
+/**
+ * An IActionResult for a bad request result.
+ *
+ * @author Jamie Martin
+ */
 public class BadRequest extends IActionResult {
+
+    // A constructor with the message in the body that gets returned to the client.
     public BadRequest(String message) {
         super(Status.BAD_REQUEST, message);
     }
--- src/main/java/common/router/NotFound.java
+++ src/main/java/common/router/NotFound.java
@@ -1,10 +1,12 @@
 package common.router;
 
+/**
+ * An IActionResult for a not found result.
+ *
+ * @author Jamie Martin
+ */
 public class NotFound extends IActionResult {
-    public NotFound() {
-        super(Status.NOT_FOUND);
-    }
-
+    // A constructor with the message in the body that gets returned to the client.
     public NotFound(String message) {
         super(Status.NOT_FOUND, message);
     }
