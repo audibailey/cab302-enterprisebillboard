743349e08d269da15208002611ac28626d509bcb
Perdana Bailey <perdana.bailey@connect.qut.edu.au>
2020-05-10 15:05:04 +1000

    feat: New class for a not found IActionResult with a method.

--- src/main/java/common/router/NotFound.java
+++ src/main/java/common/router/NotFound.java
@@ -1,5 +1,11 @@
 package common.router;
 
 public class NotFound extends IActionResult {
-    public NotFound() { super(Status.NOT_FOUND); }
+    public NotFound() {
+        super(Status.NOT_FOUND);
+    }
+
+    public NotFound(String message) {
+        super(Status.NOT_FOUND, message);
+    }
 }
