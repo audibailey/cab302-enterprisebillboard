3cfdb2525fcc85a55b8df0051bf50f2f67ad07c7
Kevin <jack6969x@gmail.com>
2020-05-10 13:52:48 +1000

    feat: Added User methods

--- src/main/java/server/controllers/user/Delete.java
+++ src/main/java/server/controllers/user/Delete.java
@@ -1,12 +1,14 @@
 package server.controllers.user;
 
 import common.models.Billboard;
+import common.models.User;
 import server.router.Action;
 import server.router.Request;
 import server.router.models.BadRequest;
 import server.router.models.IActionResult;
 import server.router.models.NotFound;
 import server.router.models.Ok;
+import server.sql.Collection;
 import server.sql.CollectionFactory;
 
 public class Delete extends Action {
@@ -16,6 +18,10 @@ public class Delete extends Action {
     @Override
     public IActionResult execute(Request req) throws Exception {
         // DELETE PERMS TOO
+        if (req.body instanceof User) {
+            CollectionFactory.getInstance(User.class).delete((User) req.body);
+            return new Ok();
+        }
         return new NotFound();
     }
 
