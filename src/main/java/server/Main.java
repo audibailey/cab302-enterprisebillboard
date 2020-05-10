cdb3104b4e8e5c9969b8e11728081aa9366c8527
Perdana Bailey <perdana.bailey@connect.qut.edu.au>
2020-05-10 15:49:15 +1000

    feat: Read port from network.props file.

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -6,9 +6,17 @@ import server.middleware.*;
 import server.router.*;
 import server.services.DataService;
 import server.sql.CollectionFactory;
+import server.sql.SchemaBuilder;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * This class is the main class, used as the entry point for the server application.
@@ -57,15 +65,23 @@ public class Main {
             .ADD("/schedule/getbyid", Authentication.Authenticate.class, ScheduleController.GetById.class)
             .ADD("/schedule/insert", Authentication.Authenticate.class, ScheduleController.Insert.class)
             .ADD("/schedule/delete", Authentication.Authenticate.class, ScheduleController.Delete.class)
-             //Add Permission actions to router
+            //Add Permission actions to router
             .ADD("/permission/get", Authentication.Authenticate.class, PermissionController.Get.class)
             .ADD("/permission/getbyid", Authentication.Authenticate.class, PermissionController.GetById.class)
             .ADD("/permission/insert", Authentication.Authenticate.class, PermissionController.Insert.class)
             .ADD("/permission/update", Authentication.Authenticate.class, PermissionController.Update.class);
 
+        // Fetch the port from the props file
+        Properties props = getProps();
+        String port = props.getProperty("server.port");
+        if (port == null) {
+            throw new Exception("Server Port was not specified in the network.props file!");
+        }
+
         // Open the socket
-        System.out.println("Opening Server on port 12345...");
-        ServerSocket ss = new ServerSocket(12345);
+        System.out.println("Opening Server on port " + port + "...");
+        int portNum = Integer.parseInt(port);
+        ServerSocket ss = new ServerSocket(portNum);
         System.out.println("Sever available at " + ss.getLocalSocketAddress());
 
         // Loop through constantly looking for connections
@@ -75,5 +91,43 @@ public class Main {
             System.out.println("A request attempt has been made from " + s.getInetAddress());
             new Thread(new SocketHandler(s, router)).start();
         }
+    }
+
+    /**
+     * Gets the properties from network.props for the socket port.
+     *
+     * @return Properties: The port.
+     * @throws IOException:          Thrown when props file not found or when unable to read props file or close prop file stream.
+     * @throws NullPointerException: Thrown when props file stream is empty.
+     */
+    private static Properties getProps() throws IOException, NullPointerException {
+
+        // Initialize variables
+        Properties props = new Properties();
+        FileInputStream in = null;
+
+        // Read the props file into the properties object
+        try {
+            in = new FileInputStream("./network.props");
+            props.load(in);
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException("network.props not in root of directory.");
+        } catch (IOException e) {
+            throw new IOException("Error reading network.props.", e);
+        } catch (NullPointerException e) {
+            throw new NullPointerException("No configuration information in network.props file.");
+        } finally {
+            // Close file stream
+            try {
+                in.close();
+            } catch (IOException e) {
+                throw new IOException("Error closing network.props.", e);
+            } catch (NullPointerException e) {
+                throw new NullPointerException("Error closing network.props. network.props doesn't exist anymore.");
+            }
+        }
+
+        // Return the properties object
+        return props;
     }
 }
