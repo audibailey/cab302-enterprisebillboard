6a4ce39230a0b9a1769a74911bed3758f9a985fd
Jamie Martin <49264143+JamieKieranMartin@users.noreply.github.com>
2020-05-18 10:50:41 +1000

    TESTS: GenericRouter (String, String map)

--- src/test/java/server/router/RouterTests.java
+++ src/test/java/server/router/RouterTests.java
@@ -1,10 +1,47 @@
 package server.router;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class RouterTests {
-    // Test assigning a route and ensuring it doesn't return anything other than Ok(), etc
+
+    private static StringRouter router;
+    private static String authentication = "protected";
+
+    static class StringRouter extends GenericRouter<String, String, StringRouter> {
+        public StringRouter(String protected_route) {
+            super(protected_route);
+        }
+
+        @Override protected StringRouter getThis() { return this; }
+    }
+
+    @BeforeAll
+    public static void PrepRoutes() {
+        router = new StringRouter(authentication)
+            .ADD_AUTH("/billboard", "canEditBillboard", "a billboard");
+    }
 
     @Test
-    public void Test() throws Exception {}
+    public void TestAuthenticated() throws Exception {
+        Object[] actions = router.route("/billboard");
+        var expectedActions = new String[3];
+
+        expectedActions[0] = authentication;
+        expectedActions[1] = "canEditBillboard";
+        expectedActions[2] = "a billboard";
+
+        assert actions.length == expectedActions.length;
+
+        for (int i = 0; i < actions.length; i++) {
+            String action = (String)actions[i];
+            assert action.equals(expectedActions[i]);
+        }
+    }
+
+    @Test
+    public void TestNull() throws Exception {
+        Object[] actions = router.route("/null");
+        assert actions == null;
+    }
 }
