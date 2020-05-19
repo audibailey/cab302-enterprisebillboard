45da5f1cbfda8f5fc2825632a62a95997c8a0753
Jamie Martin <49264143+JamieKieranMartin@users.noreply.github.com>
2020-05-19 17:03:36 +1000

    refactor: add permissions to Session

--- src/main/java/common/models/Session.java
+++ src/main/java/common/models/Session.java
@@ -9,12 +9,14 @@ public class Session implements Serializable {
     public String token;
     public int userId;
     public String username;
+    public Permissions permissions;
     public LocalDateTime expireTime;
 
-    public Session(int userId, String username) {
+    public Session(int userId, String username, Permissions permissions) {
         this.token = RandomFactory.token();
         this.userId = userId;
         this.username = username;
+        this.permissions = permissions;
         this.expireTime = LocalDateTime.now().plusHours(24);
     }
 }
