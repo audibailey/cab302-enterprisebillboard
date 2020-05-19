7aab1373762cbd95f9a911a4e2e42c8a0545ec64
Jamie Martin <49264143+JamieKieranMartin@users.noreply.github.com>
2020-05-19 17:03:20 +1000

    fix: exception handle in ClientSocketFactory

--- src/main/java/common/utils/ClientSocketFactory.java
+++ src/main/java/common/utils/ClientSocketFactory.java
@@ -2,6 +2,7 @@ package common.utils;
 
 import common.router.IActionResult;
 import common.router.Request;
+import common.swing.Notification;
 
 import java.io.*;
 import java.net.Socket;
@@ -27,36 +28,44 @@ public class ClientSocketFactory {
         this.body = body;
     }
 
-    public IActionResult Connect() throws IOException, ClassNotFoundException {
-        Socket s = new Socket("127.0.0.1", 12345);
+    public IActionResult Connect() {
+        try {
+            Socket s = new Socket("127.0.0.1", 12345);
 
-        OutputStream outputStream = s.getOutputStream();
-        ObjectOutputStream oos = new ObjectOutputStream(outputStream);
-        Request req = new Request(path, token, params, body);
-        oos.writeObject(req);
-        oos.flush();
-        oos.reset();
+            OutputStream outputStream = s.getOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(outputStream);
+            Request req = new Request(path, token, params, body);
+            oos.writeObject(req);
+            oos.flush();
+            oos.reset();
 
-        InputStream inputStream = s.getInputStream();
-        ObjectInputStream ois = new ObjectInputStream(inputStream);
-        Object o = ois.readObject();
+            InputStream inputStream = s.getInputStream();
+            ObjectInputStream ois = new ObjectInputStream(inputStream);
+            Object o = ois.readObject();
 
-        IActionResult res = null;
+            IActionResult res = null;
 
-        if (o instanceof IActionResult) {
-            res = (IActionResult) o;
+            if (o instanceof IActionResult) {
+                res = (IActionResult) o;
 
-             if (res.error) {
-                //Notification.display(res.message);
-                System.out.println( res.status.toString());
-                System.out.println( res.message);
+                if (res.error) {
+                    Notification.display(res.message);
+                    System.out.println( res.status.toString());
+                    System.out.println( res.message);
+                }
             }
-        }
 
-        ois.close();
-        s.close();
-        
-        return res;
+            ois.close();
+            s.close();
+
+            return res;
+        } catch (IOException ex) {
+            Notification.display(ex.getMessage());
+            return null;
+        } catch (ClassNotFoundException ex) {
+            Notification.display(ex.getMessage());
+            return null;
+        }
     }
 
 }
