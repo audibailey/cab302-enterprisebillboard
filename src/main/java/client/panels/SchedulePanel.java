c2db8c54bf9e20d2541cf7c3b0a54b3df5311c68
Trevor Waturuocha <trevor.waturuocha@connect.qut.edu.au>
2020-05-21 10:50:52 +1000

    Feat: Schedule Frame
    Implemented basic schedule frame without components.

--- src/main/java/client/panels/SchedulePanel.java
+++ src/main/java/client/panels/SchedulePanel.java
@@ -1,26 +1,33 @@
 package client.panels;
 
+import client.frames.CreateEditUserFrame;
+import client.frames.ScheduleFrame;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class SchedulePanel extends JPanel {
-    JButton button;
+    JButton scheduleButton;
 
     public SchedulePanel() {
 
-        button = new JButton("Schedule");
-        button.addActionListener(new ActionListener() {
+        scheduleButton = new JButton("Schedule");
+        scheduleButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try {
-                    common.system.Notification.display("Hello", "test", TrayIcon.MessageType.ERROR);
-                } catch (AWTException awtException) {
-                    awtException.printStackTrace();
+//                try {
+//                    common.system.Notification.display("Hello", "test", TrayIcon.MessageType.ERROR);
+//                } catch (AWTException awtException) {
+//                    awtException.printStackTrace();
+//                }
+                // Check if schedule button is pressed
+                if(e.getSource() == scheduleButton){
+                    new ScheduleFrame(); // Open create user frame
                 }
             }
         });
-        add(button);
+        add(scheduleButton);
 
     }
 }
