8ad120b8e3f57c81667473084fe51d45d6cf321f
TrevorWats <62232658+TrevorWats@users.noreply.github.com>
2020-05-23 17:48:44 +1000

    Feat: Dialog box for Schedule Panel
    Implementing feature for schedule panel that creates a dialog to insert schedule entries into the table.

--- src/main/java/client/panels/SchedulePanel.java
+++ src/main/java/client/panels/SchedulePanel.java
@@ -2,16 +2,17 @@ package client.panels;
 
 import client.components.table.*;
 import client.services.BillboardService;
+import client.services.PermissionsService;
 import client.services.ScheduleService;
 import client.services.SessionService;
-import common.models.Billboard;
-import common.models.Picture;
-import common.models.Schedule;
-import common.models.Session;
+import common.models.*;
+import common.swing.Notification;
+import common.utils.HashingFactory;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -92,5 +93,63 @@ public class SchedulePanel extends JPanel implements ActionListener {
     @Override
     // Adding listener events for the user panel buttons.
     public void actionPerformed(ActionEvent e) {
+        // Check if new user button is pressed
+        if(e.getSource() == createButton) {
+            try {
+                // Setting up billboard dropdown menu
+                JComboBox billboards = new JComboBox((ComboBoxModel) BillboardService.getInstance().billboards);
+                // Setting up start time spinner
+                SpinnerDateModel startModel = new SpinnerDateModel();
+                JSpinner startTime = new JSpinner(startModel);
+                startTime.setEditor(new JSpinner.DateEditor(startTime,"dd.MM.yyyy"));
+                // Setting up duration spinner
+                Integer value = 1;
+                Integer min = 1;
+                Integer durMax = 30;
+                Integer step = 1;
+                SpinnerNumberModel durModel = new SpinnerNumberModel(value, min, durMax, step);
+                JSpinner duration = new JSpinner(durModel);
+                // Setting up interval spinner
+                Integer intMax = 60;
+                SpinnerNumberModel intModel = new SpinnerNumberModel(value, min, intMax, step);
+                JSpinner interval = new JSpinner(intModel);
+                // Setting up components for Schedule dialog box
+                final JComponent[] components = new JComponent[]{
+                    new JLabel("Select a billboard:"),
+                    billboards,
+                    new JLabel("Billboard start time:"),
+                    startTime,
+                    new JLabel("Billboard duration:"),
+                    duration,
+                    new JLabel("Billboard interval:"),
+                    interval
+                };
+                int result = JOptionPane.showConfirmDialog(this, components, "Schedule a billboard", JOptionPane.PLAIN_MESSAGE);
+                // If OK button is clicked, update schedule table
+                if (result == JOptionPane.OK_OPTION) {
+                    // If billboard is not selected, display warning message
+                    if (billboards.getSelectedItem() == null) {
+                        Notification.display("Billboard was not selected. Please try again");
+                        // If billboard is not selected, display warning message
+                    } else if (startTime.getValue() == null || duration == null || interval == null ) {
+                        Notification.display("One of the schedule values are empty. Please try again");
+                        // Else populate table
+                    } else {
+                        Schedule schedule = new Schedule();
+                        schedule.billboardName = (((String[])billboards.getSelectedItem())[0]);
+                        schedule.startTime = (Instant) startTime.getValue();
+                        schedule.duration = (Integer) duration.getValue();
+                        schedule.interval = (Integer) interval.getValue();
+
+                        tableModel.setObjectRows(ScheduleService.getInstance().insert(schedule));
+                        tableModel.fireTableDataChanged();
+                    }
+                }
+            }
+
+            catch (Exception ex) {
+                Notification.display(ex.getMessage());
+            }
+        }
     }
 }
