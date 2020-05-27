545f094436ca59a009f5a172088e31dc939cdb89
Jamie Martin <49264143+JamieKieranMartin@users.noreply.github.com>
2020-05-27 20:37:52 +1000

    commnet: Day

--- src/main/java/common/models/Day.java
+++ src/main/java/common/models/Day.java
@@ -2,6 +2,11 @@ package common.models;
 
 import java.util.List;
 
+/**
+ * This class consists of the Day object and its associated methods.
+ *
+ * @author Jamie Martin
+ */
 public class Day {
     public String day;
     public List<String> times;
