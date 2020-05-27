d0444d1cb68a2d0d0d43daf3c3515604a4724d70
Jamie Martin <jamie.martin@connect.qut.edu.au>
2020-05-27 20:19:58 +1000

    fix: schedule billboard name not unique

--- src/main/java/common/models/Schedule.java
+++ src/main/java/common/models/Schedule.java
@@ -24,7 +24,7 @@ public class Schedule implements Serializable, Editable {
     @SQLITE(type="INTEGER PRIMARY KEY AUTOINCREMENT")
     public int id;
 
-    @SQLITE(type="VARCHAR(255) NOT NULL UNIQUE")
+    @SQLITE(type="VARCHAR(255) NOT NULL")
     public String billboardName;
 
     // 0 - 7
