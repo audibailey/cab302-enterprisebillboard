21815d860ba502ec947b40d68422d43ee54e9392
Jamie Martin <49264143+JamieKieranMartin@users.noreply.github.com>
2020-05-29 14:20:04 +1000

    refactor: TokenService

--- src/test/java/server/services/TokenServiceTests.java
+++ src/test/java/server/services/TokenServiceTests.java
@@ -5,6 +5,13 @@ import org.junit.jupiter.api.Test;
 public class TokenServiceTests {
     // Test assigning Tokens, generating hashes, mock logging out, etc
 
+
+    // TODO: Create a new session, truthy verify, falsy verify
+
+    // TODO: Test expired
+
+    // TODO: Test nullables
+
     @Test
     public void Test() throws Exception {}
 }
