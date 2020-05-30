25548d0180ea2f92aaf4d0560796c0ac9a8f362c
Jamie Martin <49264143+JamieKieranMartin@users.noreply.github.com>
2020-05-30 20:07:51 +1000

    fix: flip test actual + expected

--- src/test/java/common/sql/StatementBuilderTests.java
+++ src/test/java/common/sql/StatementBuilderTests.java
@@ -11,25 +11,25 @@ public class StatementBuilderTests {
     @Test
     public void GetStatement() throws Exception {
         String stmt = StatementBuilder.createGetStatement(Billboard.class);
-        assertEquals(stmt, "SELECT * FROM BILLBOARD");
+        assertEquals("SELECT * FROM BILLBOARD", stmt);
     }
 
     @Test
     public void InsertStatement() throws Exception {
         String stmt = StatementBuilder.createInsertStatement(Billboard.class);
-        assertEquals(stmt, "INSERT INTO BILLBOARD (name, message, messageColor, picture, backgroundColor, information, informationColor, locked, userId) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        assertEquals("INSERT INTO BILLBOARD (name, message, messageColor, picture, backgroundColor, information, informationColor, locked, userId) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", stmt);
     }
 
     @Test
     public void UpdateStatement() throws Exception {
         String stmt = StatementBuilder.createUpdateStatement(Billboard.class);
-        assertEquals(stmt, "UPDATE BILLBOARD SET name = ?, message = ?, messageColor = ?, picture = ?, backgroundColor = ?, information = ?, informationColor = ?, locked = ?, userId = ? WHERE ID = ?");
+        assertEquals("UPDATE BILLBOARD SET name = ?, message = ?, messageColor = ?, picture = ?, backgroundColor = ?, information = ?, informationColor = ?, locked = ?, userId = ? WHERE ID = ?", stmt);
     }
 
     @Test
     public void DeleteStatement() throws Exception {
         String stmt = StatementBuilder.createDeleteStatement(Billboard.class);
-        assertEquals(stmt, "DELETE FROM BILLBOARD WHERE ID = ?");
+        assertEquals("DELETE FROM BILLBOARD WHERE ID = ?", stmt);
     }
 
 }
